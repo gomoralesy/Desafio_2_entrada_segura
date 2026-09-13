aa01e061b79d289ab9fef90f28391afe071e5fdf
gomoralesy <go.moralesy@duocuc.cl>
2026-09-13 00:03:34 -0300

    Inicio desafio 2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su edad: ");
+        int edad = scanner.nextInt();
+        System.out.println("Edad registrada: "+ edad);
+    }
+}

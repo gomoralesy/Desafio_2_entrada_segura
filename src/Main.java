f37c1d53c4c6064af1a88e38a47a8ff495f3f921
gomoralesy <go.moralesy@duocuc.cl>
2026-09-15 21:51:23 -0300

    Try-002

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+    try {
         System.out.println("Ingrese su edad: ");
         int edad = scanner.nextInt();
-        System.out.println("Edad registrada: "+ edad);
+        System.out.println("Edad registrada: " + edad);
+    }
+    catch (InputMismatchException e) {
+        System.out.println("Error de entrada incorrecta");
+    }
     }
 }

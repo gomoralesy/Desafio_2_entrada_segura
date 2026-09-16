6bb13fcde715b48d9968db6f09ebfaa33e6dcefc
gomoralesy <go.moralesy@duocuc.cl>
2026-09-15 22:04:46 -0300

    Try-003

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
     try {
         System.out.println("Ingrese su edad: ");
         int edad = scanner.nextInt();
         System.out.println("Edad registrada: " + edad);
+
+        if  (edad >= 18) {
+            System.out.println("Acceso autorizado para: "+nombre);
+        } else {
+            System.out.println("Acceso denegado.");
+        }
     }
     catch (InputMismatchException e) {
-        System.out.println("Error de entrada incorrecta");
+        System.out.println("La edad debe ser un número.");
     }
     }
 }
